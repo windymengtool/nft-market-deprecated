504928309bd5993de71a559c8f6c9181a1a39062
gust <2414566866@qq.com>
2025-04-08 17:16:15 +0800

    20250408_171615

--- practical/EasySwapContract/scripts/deploy_721.js
+++ practical/EasySwapContract/scripts/deploy_721.js
@@ -4,17 +4,18 @@ async function main() {
     const [deployer] = await ethers.getSigners()
     console.log("deployer: ", deployer.address)
 
-    // let TestERC721 = await ethers.getContractFactory("Troll")
-    // const testERC721 = await TestERC721.deploy()
-    // await testERC721.deployed()
-    // console.log("testERC721 contract deployed to:", testERC721.address)
+    // 部署 NFT 合约
+    let TestERC721 = await ethers.getContractFactory("TestERC721")
+    const testERC721 = await TestERC721.deploy()
+    await testERC721.deployed()
+    console.log("testERC721 contract deployed to:", testERC721.address)
 
     //mint
-    let testERC721Address = "0xF2e0BA02a187F19F5A390E4f990c684d81A833A0";
-    let testERC721 = await (await ethers.getContractFactory("Troll")).attach(testERC721Address)
-    tx = await testERC721.mint(deployer.address, 50);
-    await tx.wait()
-    console.log("mint tx:", tx.hash)
+    // let testERC721Address = "0xF2e0BA02a187F19F5A390E4f990c684d81A833A0";
+    // let testERC721 = await (await ethers.getContractFactory("Troll")).attach(testERC721Address)
+    // tx = await testERC721.mint(deployer.address, 50);
+    // await tx.wait()
+    // console.log("mint tx:", tx.hash)
 }
 
 // We recommend this pattern to be able to use async/await everywhere
