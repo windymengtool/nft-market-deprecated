2f7e10ec69d55d9be561c2182c2ba7955ce2d36a
gust <2414566866@qq.com>
2025-04-08 17:30:13 +0800

    20250408_173013

--- practical/EasySwapContract/scripts/deploy_721.js
+++ practical/EasySwapContract/scripts/deploy_721.js
@@ -5,17 +5,17 @@ async function main() {
     console.log("deployer: ", deployer.address)
 
     // 部署 NFT 合约
-    let TestERC721 = await ethers.getContractFactory("TestERC721")
-    const testERC721 = await TestERC721.deploy()
-    await testERC721.deployed()
-    console.log("testERC721 contract deployed to:", testERC721.address)
+    // let TestERC721 = await ethers.getContractFactory("TestERC721")
+    // const testERC721 = await TestERC721.deploy()
+    // await testERC721.deployed()
+    // console.log("testERC721 contract deployed to:", testERC721.address)
 
     //mint
-    // let testERC721Address = "0xF2e0BA02a187F19F5A390E4f990c684d81A833A0";
-    // let testERC721 = await (await ethers.getContractFactory("Troll")).attach(testERC721Address)
-    // tx = await testERC721.mint(deployer.address, 50);
-    // await tx.wait()
-    // console.log("mint tx:", tx.hash)
+    let testERC721Address = "0x4460376b7048848FDf40113e779F0b36630c9455"; // 上一步部署合约得到的地址
+    let testERC721 = await (await ethers.getContractFactory("TestERC721")).attach(testERC721Address)
+    tx = await testERC721.mint(deployer.address, 50); // 为 [deployer.address] 铸造 [50] 个 NFT
+    await tx.wait()
+    console.log("mint tx:", tx.hash)
 }
 
 // We recommend this pattern to be able to use async/await everywhere
